4b1589dbb95b255c69701f43f4761b6d06479c76
Ruben Kretek <rubenkretek@outlook.com>
2021-11-10 20:48:13 +0000

    Housekeeping: cleaned up code

--- src/components/InfoModule.js
+++ src/components/InfoModule.js
@@ -10,7 +10,6 @@ const InfoModule = () => {
     return (
         <section className="info-module">
             <div className="info-module__container" data-aos="fade-up">
-
                 <div className="info-module__image">
                     <picture>
                         <source srcset={ourStoryImage} media="(min-width: 1024px)" />
@@ -18,13 +17,11 @@ const InfoModule = () => {
                         <img src={ourStoryImageSmall} alt="Mother holding a baby, both looking happy" />
                     </picture>
                 </div>
-
                 <div className="info-module__copy">
                     <h2>Our Story</h2>
                     <p className="meta-text">In hendrerit nulla pharetra ante pulvinar egestas. Sed commodo massa id est iaculis, sit amet congue.</p>
                     <p>In hendrerit nulla pharetra ante pulvinar egestas. Sed commodo massa id est iaculis, sit amet congue enim placerat. Mauris leo arcu, mattis vitae sodales in, molestie eget quam.In hendrerit nulla pharetra ante pulvinar egestas. Sed commodo massa id est iaculis, sit amet congue enim placerat. Mauris leo arcu, mattis vitae sodales in, molestie eget quam.</p>
                 </div>
-
             </div>
         </section>
     )
