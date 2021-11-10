c88211e347db006c33dccf1f89f7982538c2ec87
Ruben Kretek <rubenkretek@outlook.com>
2021-11-10 16:40:53 +0000

    Build: Our story module

--- src/components/NewsletterModule.js
+++ src/components/NewsletterModule.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const NewsletterModule = () => {
     return (
-        <div>
+        <section className="newsletter-module">
             This is Newsletter Module
-        </div>
+        </section>
     )
 }
 
