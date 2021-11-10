7c2ab515bad3968aa2eac5b79829af3368399f58
Ruben Kretek <rubenkretek@outlook.com>
2021-11-10 19:14:53 +0000

    Build: newsletter signup box

--- src/components/NewsletterModule.js
+++ src/components/NewsletterModule.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
+//Images
+import woodBlocks from '../img/wood-blocks.svg';
+
 const NewsletterModule = () => {
     return (
         <section className="newsletter-module">
-            This is Newsletter Module
+            <div className="newsletter-module__container">
+                <div className="newsletter-module__image">
+                    <img src={woodBlocks} alt="Little Loft Logo" />
+                </div>
+                <div className="newsletter-module__content">
+                    <h2>Let’s stay in touch</h2>
+                    <p>Dicta omnes ius ut, ei atomorum voluptatum definitionem per. Zril petentium sit at, vel at quis corrumpit.</p>
+                    <form class="form form--newsletter-sign-up" action="/" method="POST">
+                        <label>Your email</label>
+                        <input type="text" placeholder="Your email..." name="mail" required />
+                        <input type="submit" value="Subscribe" />
+                    </form>
+                </div>
+            </div>
         </section>
     )
 }
